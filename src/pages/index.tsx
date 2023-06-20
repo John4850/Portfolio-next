5056ca5bd5c62038fdf08570be07fb7b4afb6b36
John4850 <johnnelson4850@gmail.com>
2023-06-20 10:58:21 -0700

    initial set up,  folder structure etc

---
+++ src/pages/index.tsx
@@ -0,0 +1,18 @@
+
+const TAGLINE = "Human Centered Tech Solutions"
+
+export default function Home() {
+  return (
+    <>
+      <title>{"John Nelson"}</title>
+      <meta name="description" content={TAGLINE} />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <link
+        rel="icon"
+        href="/icon?<generated>"
+        type="image/<generated>"
+        sizes="<generated>"
+      />
+    </>
+  )
+}
